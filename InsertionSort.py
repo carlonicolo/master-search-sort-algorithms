6c0f69e101ac7a26b269fd49fc2c773704893a91
Carlo <carlo.nicolo@hotmail.com>
2022-12-15 01:48:24 +0100

    InsertionSort

---
+++ InsertionSort.py
@@ -0,0 +1,29 @@
+import random
+
+
+def generate_random_list(n):
+    # n is the length of the list
+    my_list = []
+    for i in range(n):
+        my_list.append(random.randint(-4*n, 4*n))
+    return my_list
+
+
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        for j in range(i, 0, -1):
+            if my_list[j] < my_list[j-1]:
+                my_list[j], my_list[j-1] = my_list[j-1], my_list[j]
+            else:
+                break
+
+def main():
+    # my_list = [3, 5, 1, 9, 7, 2, 10, 4, 8, 6]
+    my_list = generate_random_list(30)
+    print(my_list)
+    insertion_sort(my_list)
+    print(my_list)
+
+
+if __name__ == "__main__":
+    main()
